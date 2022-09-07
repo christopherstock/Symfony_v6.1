6c2a59d04b762c0ecbd9cc75716f10b484200147
Christopher Stock <christopher.stock@mayflower.de>
2022-09-07 10:57:47 +0200

    Replace route with Route annotation in LuckyController. Add route '/lucky/numbers'.

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -3,15 +3,28 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class LuckyController
 {
+    #[Route('/lucky/number')]
     public function number(): Response
     {
         $number = random_int(0, 100);
 
         return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
+            '<html lang="de"><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+
+    #[Route('/lucky/numbers')]
+    public function numbers(): Response
+    {
+        $number1 = random_int(0, 100);
+        $number2 = random_int(0, 100);
+
+        return new Response(
+            '<html lang="de"><body>Lucky numbers: '.$number1.' '.$number2.'</body></html>'
         );
     }
 }
